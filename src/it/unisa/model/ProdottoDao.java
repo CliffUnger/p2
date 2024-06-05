1a8f476f2e3057f9740c4f45d5024d9d9a98f577
CliffUnger <adrianobuccino@gmail.com>
2024-06-05 11:14:54 +0200

    id-2 resolved

--- src/it/unisa/model/ProdottoDao.java
+++ src/it/unisa/model/ProdottoDao.java
@@ -154,13 +154,15 @@ public class ProdottoDao implements ProdottoDaoInterfaccia{
 		String selectSQL = "SELECT * FROM " + ProdottoDao.TABLE_NAME;
 
 		if (order != null && !order.equals("")) {
-			selectSQL += " ORDER BY " + order;
+		    selectSQL += " ORDER BY ?";
 		}
 
 		try {
-			connection = ds.getConnection();
-			preparedStatement = connection.prepareStatement(selectSQL);
-
+		    connection = ds.getConnection();
+		    preparedStatement = connection.prepareStatement(selectSQL);
+		    if (order != null && !order.equals("")) {
+		        preparedStatement.setString(1, order);
+		    }
 			ResultSet rs = preparedStatement.executeQuery();
 
 			while (rs.next()) {
