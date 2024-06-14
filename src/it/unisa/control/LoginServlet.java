b2167ae2a845cf40632fabc0f97446d71cd5e168
CliffUnger <adrianobuccino@gmail.com>
2024-06-14 17:32:15 +0200

    id-6 unresolved

--- src/it/unisa/control/LoginServlet.java
+++ src/it/unisa/control/LoginServlet.java
@@ -28,27 +28,36 @@ public class LoginServlet extends HttpServlet {
 		
 	UserDao usDao = new UserDao();
 		
-		UserBean user = new UserBean();
-		 user.setUsername(request.getParameter("un"));
-		 user.setPassword(request.getParameter("pw"));
-		 user.setPassword(PasswordHashing.hashPassword(request.getParameter("pw")));
-		   		    
-		
-		 String checkout = request.getParameter("checkout");
-		 
-		 if (user.isValid())
-		 {
-		        
-		      HttpSession session = request.getSession(true);	    
-		      session.setAttribute("currentSessionUser",user); 
-		      if(checkout!=null)
-		    	  response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
-		      
-		      else
-		    	  response.sendRedirect(request.getContextPath() + "/Home.jsp");
-		 }
-		        
-		 else 
-		      response.sendRedirect(request.getContextPath() +"/Login.jsp?action=error"); //error page 
+		try
+		{	    
+
+		     UserBean user = new UserBean();
+		     user.setUsername(request.getParameter("un"));
+		     user.setPassword(request.getParameter("pw"));
+		     user = usDao.doRetrieve(request.getParameter("un"),request.getParameter("pw"));
+			   		    
+		    
+		     String checkout = request.getParameter("checkout");
+		     
+		     if (user.isValid())
+		     {
+			        
+		          HttpSession session = request.getSession(true);	    
+		          session.setAttribute("currentSessionUser",user); 
+		          if(checkout!=null)
+		        	  response.sendRedirect(request.getContextPath() + "/account?page=Checkout.jsp");
+		          
+		          else
+		        	  response.sendRedirect(request.getContextPath() + "/Home.jsp");
+		     }
+			        
+		     else 
+		          response.sendRedirect(request.getContextPath() +"/Login.jsp?action=error"); //error page 
+		} 
+				
+				
+		catch(SQLException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
 		  }
 	}
